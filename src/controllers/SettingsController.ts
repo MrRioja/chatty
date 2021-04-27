79ab4529cc387d1cb6658ae45605cada1395c0f2
Luiz Rioja <lulyrioja@gmail.com>
2021-04-26 23:57:11 -0300

    :alien: Creating the settings service

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -1,19 +1,12 @@
 import { Request, Response } from "express";
-import { getCustomRepository } from "typeorm";
-
-import { SettingsRepository } from "../repositories/SettingsRepository";
 
 class SettingsController {
   async create(req: Request, res: Response) {
     const { chat, username } = req.body;
-    const settingsRepository = getCustomRepository(SettingsRepository);
 
-    const settings = settingsRepository.create({
-      chat,
-      username,
-    });
+    const settingsService = new SettingsService();
 
-    await settingsRepository.save(settings);
+    const settings = await settingsService.create({ chat, username });
 
     return res.json(settings);
   }
