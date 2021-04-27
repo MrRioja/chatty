eaa5a7442135a69fc5100aa85247f34a2b86060e
Luiz Rioja <lulyrioja@gmail.com>
2021-04-27 00:03:58 -0300

    :alien: Checking if the user is already registered

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { SettingsService } from "../services/SettingsService";
 
 class SettingsController {
   async create(req: Request, res: Response) {
@@ -6,9 +7,13 @@ class SettingsController {
 
     const settingsService = new SettingsService();
 
-    const settings = await settingsService.create({ chat, username });
+    try {
+      const settings = await settingsService.create({ chat, username });
 
-    return res.json(settings);
+      return res.json(settings);
+    } catch (error) {
+      return res.status(400).json({ message: error.message });
+    }
   }
 }
 
