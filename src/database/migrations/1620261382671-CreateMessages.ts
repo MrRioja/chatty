7fe502576893db8ff4e65c304d68366e55b24ea7
Luiz Rioja <lulyrioja@gmail.com>
2021-05-05 21:51:01 -0300

    :alien: Creating the message migration

---
+++ src/database/migrations/1620261382671-CreateMessages.ts
@@ -0,0 +1,53 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateMessages1620261382671 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "messages",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+
+          {
+            name: "admin_id",
+            type: "uuid",
+            isNullable: true,
+          },
+
+          {
+            name: "user_id",
+            type: "uuid",
+          },
+          {
+            name: "text",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+
+        foreignKeys: [
+          {
+            name: "FKUser",
+            columnNames: ["user_id"],
+            referencedColumnNames: ["id "],
+            referencedTableName: "users",
+            onDelete: "SET NULL",
+            onUpdate: "SET NULL",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("messages");
+  }
+}
