8c9f9c075656792f59dc6c544b903be7007c515f
Luiz Rioja <lulyrioja@gmail.com>
2023-02-05 17:00:05 -0300

    feat(messages): finish controller implementation

--- src/controllers/MessagesController.ts
+++ src/controllers/MessagesController.ts
@@ -1,27 +1,24 @@
 import { Request, Response } from "express";
-import { MessagesServices } from "../services/MessagesServices";
+import { MessagesService } from "../services/MessagesService";
 
 class MessagesController {
-  async create(req: Request, res: Response) {
+  async create(req: Request, res: Response): Promise<Response> {
     const { admin_id, text, user_id } = req.body;
-    const messagesService = new MessagesServices();
+    const messagesService = new MessagesService();
 
-    const message = await messagesService.create({
-      admin_id,
-      text,
-      user_id,
-    });
+    const message = await messagesService.create({ admin_id, text, user_id });
 
     return res.json(message);
   }
 
-  async showByUser(req: Request, res: Response) {
+  async showByUser(req: Request, res: Response): Promise<Response> {
     const { id } = req.params;
-    const messagesService = new MessagesServices();
 
-    const list = await messagesService.listByUser(id);
+    const messagesService = new MessagesService();
 
-    return res.json(list);
+    const messages = await messagesService.listByUser(id);
+
+    return res.json(messages);
   }
 }
 
